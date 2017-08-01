d53a1613f299314b4c67c62a937b08f90a604dab
shobhit-pandey <Shobhitpandey8@gmail.com>
2017-08-01 23:29:33 +0530

    reset password custom template

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^contact/$',views.contact),
     url(r'^logout/$',logout,{'template_name':'accounts/logout.html'}),
     url(r'^register/$',views.register,name='register'),
-    url(r'^reset-password/$',password_reset,name='reset_password'),
+    url(r'^reset-password/$',password_reset,{'template_name':'accounts/reset_password.html'},name='reset_password'),
     url(r'^reset-password/done$',password_reset_done,name='password_reset_done'),
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',password_reset_confirm,name='password_reset_confirm'),
     url(r'^reset-password/complete$',password_reset_complete,name='password_reset_complete'),
