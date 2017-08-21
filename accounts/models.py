c06564f45e73e8e3fafe7e68af08a1af1c1e595f
shobhit-pandey <Shobhitpandey8@gmail.com>
2017-08-21 16:08:16 +0530

    # fix userprofile with username

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,8 @@ class UserProfile(models.Model):
     website=models.URLField(default='')
     phone=models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.user.username
 
 
 def create_profile(sender,**kwargs):
